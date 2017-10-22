87507b50e1712bdbdb6818328b7a66c575300fa0
wildarch <daandegraaf9@gmail.com>
2017-10-22 17:44:07 +0200

    Clean test artifacts from git

--- test/run.sh
+++
@@ -1,8 +0,0 @@
-g++ \
-    ../src/DroneDetector.h ../src/DroneDetector.cpp \
-    Test.cpp \
-    -lboost_unit_test_framework \
-    -lopencv_core -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc\
-     -o test \
-&& ./test
-
