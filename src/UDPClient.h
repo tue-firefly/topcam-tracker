e4c261fd44836d38bdac176f12bb9e4730d7a0a5
Firefly <firefly@fireflyeindhoven.nl>
2018-01-24 16:33:40 +0100

    final version 24-01

--- src/UDPClient.h
+++ src/UDPClient.h
@@ -31,6 +31,7 @@ public:
 
 	void send(boost::asio::mutable_buffers_1 msg) {
 		std::size_t bytes_send = socket_.send_to(msg, endpoint_);
+		std::cout << "UDP endpoint address: " << endpoint_.address() << std::endl;
 		std::cout << "UDP bytes send: " << bytes_send << std::endl;
 	}
 	
@@ -45,6 +46,8 @@ public:
 		// Add CRC checksum
 		uint8_t * data_arr = (uint8_t *)data;
 		data->crc16 = ::crc_16(data_arr, sizeof(FlatState) - 2);
+
+//		std::cout << "crc: " << data->crc16 << std::endl;
 
 		return boost::asio::buffer((void *) data, sizeof(FlatState));
 	}
